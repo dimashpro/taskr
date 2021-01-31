38722fc3a2e04bb51b0412e40c94a03b86561f2e
dimashpro <sergiu.proca@gmail.com>
2021-01-31 12:20:27 +0100

    added TaskrEngine class

--- src/com/taskr/engine/TaskrEngine.java
+++ src/com/taskr/engine/TaskrEngine.java
@@ -1,2 +1,12 @@
-package com.taskr.engine;public class TaskrEngine {
+package com.taskr.engine;
+
+import com.taskr.service.TaskService;
+
+public class TaskrEngine {
+
+    private final TaskService taskService = new TaskService();
+
+    public void run(){
+
+    }
 }
