f36666a3e75ad072a3a3fd031055b4290fda8d49
dimashpro <sergiu.proca@gmail.com>
2021-02-04 21:51:47 +0100

    updated Engine

--- src/com/taskr/engine/TaskrEngine.java
+++ src/com/taskr/engine/TaskrEngine.java
@@ -15,9 +15,9 @@ public class TaskrEngine {
     public static final int VIEW_TODAY_TASKS = 1;
     public static final int VIEW_ALL_TASKS = 2;
     public static final int ADD_TASK = 3;
-    public static final int DELETE_TASK = 5;
+    public static final int DELETE_TASK = 4;
     public static final int EXIT = -1;
-    private static final int UPDATE_TASK = 4;
+  //  private static final int UPDATE_TASK = 4;
     private final TaskService taskService = new TaskService(new TaskRepository());
 
     private final Scanner scanner;
@@ -33,8 +33,7 @@ public class TaskrEngine {
             printSingleLineBoxedText("" +
                     "[" + VIEW_TODAY_TASKS + "] View today`s tasks\n" +
                     "[" + VIEW_ALL_TASKS + "] View all tasks\n" +
-                    "[" + ADD_TASK + "] Add task\n" +
-                    "[" + UPDATE_TASK + "] Update task\n" +
+                    "[" + ADD_TASK + "] Add task\n" +  //       "[" + UPDATE_TASK + "] Update task\n" +
                     "[" + DELETE_TASK + "] Delete task\n\n" +
                     "[" + EXIT + "] Exit");
             int selectedOption = scanner.nextInt();
@@ -48,9 +47,9 @@ public class TaskrEngine {
                 case ADD_TASK:
                     enterAddTaskMenu();
                     break;
-                case UPDATE_TASK:
-                    enterUpdateTaskMenu();
-                    break;
+//                case UPDATE_TASK:
+//                    enterUpdateTaskMenu();
+//                    break;
                 case DELETE_TASK:
                     enterDeleteTaskMenu();
                     break;
@@ -66,78 +65,80 @@ public class TaskrEngine {
         } while (mainMenuRunning);
     }
 
-    private void enterUpdateTaskMenu() {
-        boolean innerMenuRunning = true;
-        do {
-            printDoubleLineBoxedText("Taskr - Update task");
-            printTopCorneredSingleLine(160);
-            System.out.println("  Pick the task you want to update : \n");
-            List<Task> allTasks = taskService.getAll();
-            for (int i = 0; i < allTasks.size(); i++) {
-                System.out.println("  [" + i + "] " + allTasks.get(i));
-            }
-            System.out.println("\n  [-1] Back");
-            printBottomCorneredSingleLine(160);
-            int innerSelectedOption = scanner.nextInt();
-            if (innerSelectedOption == -1)
-                innerMenuRunning = false;
-            else if (innerSelectedOption >= 0 && innerSelectedOption < allTasks.size()) {
-                scanner.nextLine();
-                printTopCorneredSingleLine(160);
-                System.out.println("  Please complete the following form, leave empty if no change is required :");
-
-                System.out.print("   - Title : ");
-                String title = scanner.nextLine();
-
-                System.out.print("   - Description : ");
-                String description = scanner.nextLine();
-
-                System.out.print("   - Priority (HIGH/MEDIUM/LOW) : ");
-                String stringPriority = scanner.nextLine();
-                TaskPriority taskPriority = null;
-                if (!stringPriority.trim().isEmpty())
-                    taskPriority = TaskPriority.valueOf(stringPriority);
-
-                System.out.print("   - Start date (yyyy-MM-dd) : ");
-                String stringStartDate = scanner.nextLine();
-                LocalDate startDate = null;
-                if (!stringStartDate.trim().isEmpty())
-                    startDate = LocalDate.parse(stringStartDate);
-
-                System.out.print("   - End date (yyyy-MM-dd) : ");
-                String stringEndDate = scanner.nextLine();
-                LocalDate endDate = null;
-                if (!stringEndDate.trim().isEmpty())
-                    endDate = LocalDate.parse(stringEndDate);
-
-                System.out.print("   - Tags (a, a1, a2 ... n): ");
-                String stringTags = scanner.nextLine();
-                List<String> tags = null;
-                if (!stringTags.trim().isEmpty())
-                    tags = Arrays.asList(stringTags.split(", "));
-
-                Task task = new Task(title, description, tags, taskPriority, startDate, endDate);
-                task.setId(allTasks.get(innerSelectedOption).getId());
-
-                taskService.save(task);
-                System.out.println("  Successfully updated : ");
-                System.out.println(task);
-                printBottomCorneredSingleLine(160);
-            } else {
-                System.out.println("  Wrong option... [" + innerSelectedOption + "]");
-            }
-
-        } while (innerMenuRunning);
-    }
+//    private void enterUpdateTaskMenu() {
+//        boolean innerMenuRunning = true;
+//        do {
+//            printDoubleLineBoxedText("Taskr - Update task");
+//            printTopCorneredSingleLine(160);
+//            System.out.println("  Pick the task you want to update : \n");
+//            List<Task> allTasks = taskService.getAll();
+//            for (int i = 0; i < allTasks.size(); i++) {
+//                System.out.println("  [" + i + "] " + allTasks.get(i));
+//            }
+//            System.out.println("\n  [-1] Back");
+//            printBottomCorneredSingleLine(160);
+//            int innerSelectedOption = scanner.nextInt();
+//            if (innerSelectedOption == -1)
+//                innerMenuRunning = false;
+//            else if (innerSelectedOption >= 0 && innerSelectedOption < allTasks.size()) {
+//                scanner.nextLine();
+//                printTopCorneredSingleLine(160);
+//                System.out.println("  Please complete the following form, leave empty if no change is required :");
+//
+//                System.out.print("   - Title : ");
+//                String title = scanner.nextLine();
+//
+//                System.out.print("   - Description : ");
+//                String description = scanner.nextLine();
+//
+//                System.out.print("   - Priority (HIGH/MEDIUM/LOW) : ");
+//                String stringPriority = scanner.nextLine();
+//                TaskPriority taskPriority = null;
+//                if (!stringPriority.trim().isEmpty())
+//                    taskPriority = TaskPriority.valueOf(stringPriority);
+//
+//                System.out.print("   - Start date (yyyy-MM-dd) : ");
+//                String stringStartDate = scanner.nextLine();
+//                LocalDate startDate = null;
+//                if (!stringStartDate.trim().isEmpty())
+//                    startDate = LocalDate.parse(stringStartDate);
+//
+//                System.out.print("   - End date (yyyy-MM-dd) : ");
+//                String stringEndDate = scanner.nextLine();
+//                LocalDate endDate = null;
+//                if (!stringEndDate.trim().isEmpty())
+//                    endDate = LocalDate.parse(stringEndDate);
+//
+//                System.out.print("   - Tags (a, a1, a2 ... n): ");
+//                String stringTags = scanner.nextLine();
+//                List<String> tags = null;
+//                if (!stringTags.trim().isEmpty())
+//                    tags = Arrays.asList(stringTags.split(", "));
+//
+//                Task task = new Task(title, description, tags, taskPriority, startDate, endDate);
+//                task.setId(allTasks.get(innerSelectedOption).getId());
+//
+//                taskService.save(task);
+//                System.out.println("  Successfully updated : ");
+//                System.out.println(task);
+//                printBottomCorneredSingleLine(160);
+//            } else {
+//                System.out.println("  Wrong option... [" + innerSelectedOption + "]");
+//            }
+//
+//        } while (innerMenuRunning);
+//    }
 
     private void enterViewTodayTasksMenu() {
         boolean innerMenuRunning = true;
         do {
             printDoubleLineBoxedText("Taskr - View today`s tasks");
-        //    String todayTasks = taskService.getForToday().stream().map(t -> "\n" + t.toString()).reduce("", String::concat);
-            String todayTasks = taskService.getForToday().toString();
-
-            printSingleLineBoxedText("Here are your tasks for :" + LocalDate.now() + todayTasks + "\n\n[-1] Back");
+            List<Task> todayTasks = taskService.getForToday();
+            System.out.println("Your tasks for today " + LocalDate.now() + " are:");
+            for (Task todayTask : todayTasks) {
+                System.out.println(todayTask);
+            }
+            System.out.println("[-1]  back");
             int innerSelectedOption = scanner.nextInt();
             if (innerSelectedOption == -1)
                 innerMenuRunning = false;
@@ -148,10 +149,13 @@ public class TaskrEngine {
         boolean innerMenuRunning = true;
         do {
             printDoubleLineBoxedText("Taskr - View all tasks");
-           // String allTasks = taskService.getAll().stream().map(t -> "\n" + t.toString()).reduce("", String::concat);
-            String allTasks = taskService.getAll().toString();
 
-            printSingleLineBoxedText("Here are your tasks :" + allTasks + "\n\n[-1] Back");
+            List<Task> allTasks = taskService.getForToday();
+            System.out.println("All your tasks are:");
+            for (Task allTask : allTasks) {
+                System.out.println(allTask);
+            }
+            System.out.println("[-1]  back");
             int innerSelectedOption = scanner.nextInt();
             if (innerSelectedOption == -1)
                 innerMenuRunning = false;
